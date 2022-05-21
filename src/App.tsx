179d68df53eee87f97d12664606e0c2616c85c4e
antoniogdelrio <antoniogdelrio@hotmail.com>
2022-05-21 12:59:30 -0300

    Fix: hidden token

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
-import { useContext } from 'react'
-import { LoginContext } from './contexts/LoginContext'
 import Login from './pages/Login'
 
 function App() {
-  const { token } = useContext(LoginContext)
-
   return (
     <div className="App">
-      {token}
       <Login />
     </div>
   )
